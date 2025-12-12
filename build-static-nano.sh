7033294f6a8c837a4e9df61be13caa1f50155041
shengshampoo <shengzh@tutanota.com>
2025-12-12 12:52:23 +0000

    Update build-static-nano.sh

--- build-static-nano.sh
+++ build-static-nano.sh
@@ -9,9 +9,10 @@ mkdir -p /work/artifact
 
 # nano
 cd $WORKSPACE
-aa=8.7
-curl -sL https://www.nano-editor.org/dist/v$(echo $aa | sed 's/\..*//')/nano-$aa.tar.xz | tar x --xz
-cd nano-$aa
+a1=$(git ls-remote --tags git://git.git.savannah.gnu.org/nano.git | grep -Po 'refs/tags/\K[v0-9]+' | tail -n 1)
+a2=$(git ls-remote --tags git://git.git.savannah.gnu.org/nano.git | grep -Po 'refs/tags/v\K[0-9.]+' | tail -n 1)
+curl -sL https://www.nano-editor.org/dist/$a1/nano-$a2.tar.xz | tar x --xz
+cd nano-$a2
 LDFLAGS="-static -no-pie -s" LIBS="-lz" ./configure --prefix=/usr/local/nanomm --enable-libmagic --disable-wrapping --enable-utf8 --enable-static
 make
 make install

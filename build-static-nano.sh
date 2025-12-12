f7b68f75a93e31bc1985f1311c5192a26c74958f
shengshampoo <shengzh@tutanota.com>
2025-12-12 12:43:58 +0000

    Update build-static-nano.sh

--- build-static-nano.sh
+++ build-static-nano.sh
@@ -10,7 +10,7 @@ mkdir -p /work/artifact
 # nano
 cd $WORKSPACE
 aa=8.7
-curl -sL https://www.nano-editor.org/dist/v8/nano-$aa.tar.xz | tar x --xz
+curl -sL https://www.nano-editor.org/dist/v$(echo $aa | sed 's/\..*//')/nano-$aa.tar.xz | tar x --xz
 cd nano-$aa
 LDFLAGS="-static -no-pie -s" LIBS="-lz" ./configure --prefix=/usr/local/nanomm --enable-libmagic --disable-wrapping --enable-utf8 --enable-static
 make
